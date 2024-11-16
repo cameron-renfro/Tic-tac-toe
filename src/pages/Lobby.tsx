6e56dd1c246fe79fefa6d1fabf30216d99c6f1c9
Cameron Renfro <cameron@cameronclarkrenfro.com>
2024-11-15 18:15:15 -0800

    chore: setup prettier and updated tailwind config

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { Button } from '../components/ui/button.tsx';
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from '../components/ui/form.tsx';
+// import { Input } from '@/components/ui/input';
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters',
+  }),
+});
 
 const Lobby = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: 'What is your username?',
+    },
+  });
   return (
-    <>
-      <h1>Tic-Tac-Toe</h1>
-    </>
+    <Form>
+      {/* <FormField
+        control={'...'}
+        name='...'
+        render={() => (
+          <FormItem>
+            <FormLabel />
+            <FormControl>{form}</FormControl>
+            <FormDescription />
+            <FormMessage />
+          </FormItem>
+        )}
+      /> */}
+    </Form>
   );
 };
 
