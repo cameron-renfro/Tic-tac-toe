eb10df270273f073815d3f70da02a3456213b9d6
Cameron Renfro <cameron@cameronclarkrenfro.com>
2024-11-18 19:15:09 -0800

    chore: setting up inital form

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,51 +1,58 @@
-import React from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import { Button } from '@/components/ui/button';
-// import {
-//   Form,
-//   FormControl,
-//   FormDescription,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from '../components/ui/form.tsx';
-import { Input } from '../components/ui/input.tsx';
+import { Input } from '@/components/ui/input';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 
-// const formSchema = z.object({
-//   username: z.string().min(2, {
-//     message: 'Username must be at least 2 characters',
-//   }),
-// });
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters',
+  }),
+});
 
 const Lobby = () => {
-  // const form = useForm<z.infer<typeof formSchema>>({
-  //   resolver: zodResolver(formSchema),
-  //   defaultValues: {
-  //     username: 'What is your username?',
-  //   },
-  // });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: 'What is your username?',
+    },
+  });
+
+  const handleSubmit = () => {
+    // To-do: Setup Submit handler
+    // To-do: Update Fields
+    // To-do: Update Styles
+    // To-do: Add secondary field
+    // To-do: Determine how data will Persist
+  };
+
   return (
-    <>
-      <Button />
-      <Input />
-    </>
-    // <Form>
-    /* <FormField
-        control={'...'}
-        name='...'
-        render={() => (
-          <FormItem>
-            <FormLabel />
-            <FormControl>{form}</FormControl>
-            <FormDescription />
-            <FormMessage />
-          </FormItem>
-        )}
-      /> */
-    // </Form>
+    <Form {...form}>
+      <form onSubmit={handleSubmit}>
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel />
+              <FormControl>
+                <Input placeholder='' {...field} />
+              </FormControl>
+              <FormDescription />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
   );
 };
 
